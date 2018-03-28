97de8f7003907454fb5f30a15304806f1b803ab2
amanddali <league@WTS22.attlocal.net>
2018-03-27 18:24:22 -0700

    more alg. today

--- src/algorithm/Algorithms.java
+++ src/algorithm/Algorithms.java
@@ -16,14 +16,42 @@ public class Algorithms {
 
 	public static Object countPearls(List<Boolean> oysters) {
 		// TODO Auto-generated method stub
-
+		int x = 0;
 		for (int i = 0; i < oysters.size(); i++) {
-			if (oysters.get(i).booleanValue() == true) {
-
+			if (oysters.get(i) == true) {
+				x++;
 			}
 
 		}
-		return null;
+		return x;
 	}
 
+	public static double findTallest(List<Double> peeps) {
+		// TODO Auto-generated method stub
+		double p = peeps.get(0);
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > p) {
+				p = peeps.get(i);
+			}
+		}
+		return p;
+	}
+
+	public static Object findLongestWord(List<String> words) {
+		// TODO Auto-generated method stub
+		String s = words.get(0);
+		for (int j = 1; j < words.size(); j++) {
+			if (words.get(j).length() > s.length()) {
+				s = words.get(j);
+			}
+		}
+		return s;
+		// int i = 0;
+		// for (int j = 0; j < words.size(); j++) {
+		// if (words.get(j).length() > words.get(i).length()) {
+		// i = j;
+		// }
+		// }
+		// return words.get(i);
+	}
 }
